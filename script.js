eaa01a62dd45005251050b963af1c198919e94f3
jonathan <jonjonnjonjonn@gmail.com>
2021-10-14 18:37:08 -0500

    can choose how many squares and they automatically fit the container dimensions

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
 
 
-// creating the main outline
-const body = document.querySelector(`body`)
+    let size = 16
 
+originalSize() //starts out with 16x16
+
+//CREATING RESET BUTTON
+
+const reset = document.getElementById('reset')
+reset.textContent = `CHOOSE THE SIZE   `
+
+reset.addEventListener(`click`, chooseSize)
 
 // adding the classes
 
 
-for (i = 0; i < 16; i++) {
-    // creates the rows
-    const row = document.createElement(`div`)
-    row.classList = `row`
-    body.appendChild(row)
 
-    // creates the columns
-    for (j = 0; j < 16; j++) {
-        const squares = document.createElement(`div`)
-        squares.classList = `square`
-        row.appendChild(squares)
-    }
+function chooseSize() {
+    size = prompt(`what is your size`)  // gets L X H
+
+    const rows = Array.from(document.getElementsByClassName(`row`)) //grabs rows and puts into an aray
+    rows.forEach(row => row.remove())  // deletes existing rows
+
+    originalSize()
+
 }
 
-const allSquares = Array.from(document.getElementsByClassName(`square`))
 
-console.log(allSquares)
+function originalSize(e) {
 
-allSquares.forEach(square => {
+    // creating the main outline
+    const body = document.querySelector(`body`);
+    const container = document.getElementById(`container`);
 
-    square.addEventListener(`mouseover`, changeColor)
-    square.style.background = `white`
+    for (i = 0; i < size; i++) {
+        // creates the rows
+        const row = document.createElement(`div`)
+        row.classList = `row`
+        container.appendChild(row)
 
+        // creates the columns
+        for (j = 0; j < size; j++) {
+            const squares = document.createElement(`div`)
+            squares.classList = `square`
+            squares.style.height = `${600/size}px` // makes squares fit the 600px dimensions
+            squares.style.width = `${600/size}px`  // makes squares fit the 600px dimensions
+            row.appendChild(squares)
+        }
+    }
 
-    function changeColor(e) {
-        // if (mouseUp != mouseDown) {
+    const allSquares = Array.from(document.getElementsByClassName(`square`)) //grabs all squares and puts them into an array
+
+    // makes squares change colors
+    allSquares.forEach(square => {
+
+        square.addEventListener(`mouseover`, changeColor)
+        square.style.background = `white`
+
+        function changeColor(e) {
+            // if (mouseUp != mouseDown) {
             if (square.style.backgroundColor === `white`) {
                 square.style.background = `red`
             } else if (square.style.backgroundColor === `red`) {
@@ -47,27 +72,8 @@ allSquares.forEach(square => {
                 square.style.background = `violet`
             } else if (square.style.backgroundColor === `violet`) {
                 square.style.background = `white`
-            } 
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-
-        // }
-
-
-    }
-})
+            }
+            // }
+        }
+    })
+}
