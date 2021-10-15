4bef0b0e67dcf7fde62c0dcb4fe7d0d468e8aac8
jonathan <jonjonnjonjonn@gmail.com>
2021-10-14 20:51:59 -0500

    looks fine, works fine. could add color choices, dimension slider, and update button styles

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ let size = 16
 originalSize() //starts out with 16x16 and rainbow colors
 let makeRainbowON = true;
 let makeTwoColorsON = false
-//CREATING RESET BUTTON
+// SIZE BUTTON
 
 const reset = document.getElementById('reset')
-reset.textContent = `CHOOSE THE SIZE   `
 reset.addEventListener(`click`, chooseSize)
+
+// TWO COLOR BUTTON
+const twoColorsBtn = document.getElementById('twoColors')
+twoColorsBtn.addEventListener(`click`, makeTwoColors)
+
+// RAINBOW BUTTON
+const rainbowBtn = document.getElementById('rainbow')
+rainbowBtn.addEventListener(`click`, makeRainbow)
+
+// CLEAR BUTTON
+const clearBtn = document.getElementById('clear')
+clearBtn.addEventListener(`click`, resetColors)
 
 // adding the classes
 
@@ -27,7 +38,7 @@ reset.addEventListener(`click`, chooseSize)
 
 
 function originalSize(e) { // creates main outline for grid
-    
+
     const body = document.querySelector(`body`);
     const container = document.getElementById(`container`);
     for (i = 0; i < size; i++) {
@@ -47,38 +58,30 @@ function originalSize(e) { // creates main outline for grid
     }
 }
 
+function replaceRows() {
+    const rows = Array.from(document.getElementsByClassName(`row`)) //grabs rows and puts into an aray
+    rows.forEach(row => row.remove())  // deletes existing rows
+
+    originalSize()
+}
 
 function chooseSize() {
     size = prompt(`what is your size`)  // gets L X H
 
-    const rows = Array.from(document.getElementsByClassName(`row`)) //grabs rows and puts into an aray
-    rows.forEach(row => row.remove())  // deletes existing rows
-
-    originalSize()
-
+    if (size>100 || isNaN(size) || size<1) {
+        alert(`Please select a number 1-100.`)
+        chooseSize()
+    }
+    replaceRows()
+    checkColorPattern()
 }
 
-
-
-
-
-const twoColors = document.getElementById('twoColors')
-twoColors.addEventListener(`click`, changeColors)
-
-
-function changeColors(e) {
-    const rows = Array.from(document.getElementsByClassName(`row`)) //grabs rows and puts into an aray
-    rows.forEach(row => row.remove())  // deletes existing rows
-
-    originalSize()
-    makeTwoColors()
-}
-
-makeRainbow() //makes page start out as rainbow
 
 function makeRainbow() {
     makeRainbowON = true
     makeTwoColorsON = false
+
+    replaceRows()
     const allSquares = Array.from(document.getElementsByClassName(`square`)) //grabs all squares and puts them into an array
 
     allSquares.forEach(square => {
@@ -112,19 +115,36 @@ function makeTwoColors() {
     makeRainbowON = false
     makeTwoColorsON = true
 
+    replaceRows()
+
     const allSquares = Array.from(document.getElementsByClassName(`square`)) //grabs all squares and puts them into an array
 
     allSquares.forEach(square => {
 
-            square.addEventListener(`mouseover`, abcd)
-            square.style.background = `white`
+        square.addEventListener(`mouseover`, abcd)
+        square.style.background = `white`
 
-            function abcd(e) {
-                if (square.style.backgroundColor === `white`) {
-                    square.style.background = `black`
-                } else if (square.style.backgroundColor === `black`) {
-                    square.style.background = `white`
-                }
+        function abcd(e) {
+            if (square.style.backgroundColor === `white`) {
+                square.style.background = `black`
+            } else if (square.style.backgroundColor === `black`) {
+                square.style.background = `white`
             }
-        })
-            }    
+        }
+    })
+}
+
+function checkColorPattern() {
+    if (makeRainbowON === true) {
+        makeRainbow()
+    } else if (makeTwoColorsON === true)
+        makeTwoColors()
+}
+
+function resetColors() {
+    replaceRows()
+    checkColorPattern()
+
+}
+
+makeRainbow() // IMPORTANT makes page start out as rainbow
