79a136013584be4f8186b90d0d806058b0a4c429
jonathan <jonjonnjonjonn@gmail.com>
2021-10-16 18:11:49 -0500

    finished?

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 let size = 16
-originalSize() //starts out with 16x16 and rainbow colors
 let makeRainbowON = true;
 let makeTwoColorsON = false
 let makeSlowlyDarkerON = false
+let makeRandomColorON = false
+
+
+
 // SIZE SLIDER
 
 const slider = document.getElementById("myRange");
@@ -32,9 +35,14 @@ slowerDarkerBtn.addEventListener(`click`, makeSlowlyDarker)
 const rainbowBtn = document.getElementById('rainbow')
 rainbowBtn.addEventListener(`click`, makeRainbow)
 
+// RANDOM RAINBOW BUTTON
+const randomColorBtn = document.getElementById(`random`)
+randomColorBtn.addEventListener(`click`, makeRandomColor)
+
 // CLEAR BUTTON
 const clearBtn = document.getElementById('clear')
 clearBtn.addEventListener(`click`, resetColors)
+
 
 // adding the classes
 
@@ -54,7 +62,6 @@ clearBtn.addEventListener(`click`, resetColors)
 
 
 function originalSize(e) { // creates main outline for grid
-
     const body = document.querySelector(`body`);
     const container = document.getElementById(`container`);
     for (i = 0; i < size; i++) {
@@ -72,29 +79,33 @@ function originalSize(e) { // creates main outline for grid
             row.appendChild(squares)
         }
     }
+    changeBtnBackground()
+
 }
 
 function replaceRows() {
     const rows = Array.from(document.getElementsByClassName(`row`)) //grabs rows and puts into an aray
     rows.forEach(row => row.remove())  // deletes existing rows
-
     originalSize()
 }
 
 function chooseSize(e) {
     size = slider.value  // gets L X H
 
-    setTimeout(() => {replaceRows()  // using setTimeout to help with lag on slider? Does it work?
+    setTimeout(() => {
+        replaceRows()  // using setTimeout to help with lag on slider? Does it work?
         checkColorPattern()
 
-        
-    }, 200); 
+
+    }, 200);
 }
 
 function makeRainbow() {
     makeRainbowON = true
     makeTwoColorsON = false
     makeSlowlyDarkerON = false
+    makeRandomColorON = false
+
 
 
     replaceRows()
@@ -130,8 +141,9 @@ function makeRainbow() {
 function makeTwoColors() {
     makeRainbowON = false
     makeSlowlyDarkerON = false
-
     makeTwoColorsON = true
+    makeRandomColorON = false
+
 
     replaceRows()
 
@@ -156,6 +168,7 @@ function makeSlowlyDarker() {
     makeSlowlyDarkerON = true
     makeRainbowON = false
     makeTwoColorsON = false
+    makeRandomColorON = false
 
     replaceRows()
 
@@ -164,46 +177,121 @@ function makeSlowlyDarker() {
     allSquares.forEach(square => {
 
         square.addEventListener(`mouseover`, abcd)
-        square.style.background = `black`
-        square.style.opacity = `0`
-        let num = .1
-        console.log( square.style.opacity === `0` )
+        square.style.backgroundColor = `white`
+        let num = 0
         function abcd(e) {
+            if (square.style.backgroundColor === `white`) {
+                square.style.backgroundColor = `black`
+                num += .1
+                square.style.opacity = num
 
-            if (square.style.opacity === `0`) {
+            } else if (square.style.opacity < 1) {
+                num += .1
+                square.style.opacity = num
 
-                square.style.opacity += num.toFixed(1);
-console.log(+square.style.opacity === `.1`)
-            } else if (square.style.opacity === `.1`) {
-                square.style.opacity += num.toFixed(1);
-                console.log( square.style.opacity)
+            } else if (square.style.opacity >= 1) {
+                square.style.backgroundColor = `white`
+                num = 0
+                square.style.opacity = num
 
-            } else if (square.style.opacity === `.2`) {
-                square.style.opacity += num.toFixed(1);
-            } else if (square.style.opacity === `.3`) {
-                square.style.opacity += num.toFixed(1);
-            } else if (square.style.opacity === `.4`) {
-                square.style.opacity += num.toFixed(1);
-            } else if (square.style.opacity === `.5`) {
-                square.style.opacity += num.toFixed(1);
-            } 
-        
-    }})
+            }
+
+        }
+    })
 }
+
+function makeRandomColor() {
+    makeRandomColorON = true
+    makeRainbowON = false
+    makeTwoColorsON = false
+    makeSlowlyDarkerON = false
+
+    function randomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    replaceRows()
+    const allSquares = Array.from(document.getElementsByClassName(`square`)) //grabs all squares and puts them into an array
+
+    allSquares.forEach(square => {
+
+        square.addEventListener(`mouseover`, changeColor)
+        square.style.background = `white`
+
+        function changeColor(e) {
+            square.style.background = randomColor()
+
+        }
+    })
+
+
+}
+
 
 function checkColorPattern() {
     if (makeRainbowON === true) {
         makeRainbow()
-    } else if (makeTwoColorsON === true)
+    } else if (makeTwoColorsON === true) {
         makeTwoColors()
+    } else if (makeSlowlyDarkerON === true) {
+        makeSlowlyDarker()
+    } else if (makeRandomColorON === true) {
+        makeRandomColor()
+    }
 }
+
 
 function resetColors() {
     replaceRows()
     checkColorPattern()
+    const allSquares = Array.from(document.getElementsByClassName(`square`)) //grabs all squares and puts them into an array
 
+    allSquares.forEach(square => {
+        square.style.background = `white`
+    })
 }
 
-makeRainbow() // IMPORTANT makes page start out as rainbow
 
+
+// change button background
+
+
+function changeBtnBackground() {
+    if (makeTwoColorsON === true) {
+        twoColorsBtn.classList.add(`active`)
+    }
+    else if (makeTwoColorsON === false) {
+        twoColorsBtn.classList.remove(`active`)
+    }
+
+    if (makeSlowlyDarkerON === true) {
+        slowerDarkerBtn.classList.add(`active`)
+    }
+    else if (makeSlowlyDarkerON === false) {
+        slowerDarkerBtn.classList.remove(`active`)
+    }
+
+    if (makeRainbowON === true) {
+        rainbowBtn.classList.add(`active`)
+    }
+    else if (makeRainbowON === false) {
+        rainbowBtn.classList.remove(`active`)
+    }
+
+    if (makeRandomColorON === true) {
+        randomColorBtn.classList.add(`active`)
+    }
+    else if (makeRandomColorON === false) {
+        randomColorBtn.classList.remove(`active`)
+    }
+}
+
+
+originalSize() // IMPORTANT starts out with 16x16
+makeRainbow() // IMPORTANT makes page start out as rainbow
 
